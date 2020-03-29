1147c249ab507ac738c7d5adce33e436f23e1e10
Wis <im@wis.am>
2020-03-30 02:51:29 +0300

    refactor, change config, add shortcuts for move, left,middle,right click

--- main.cpp
+++ main.cpp
@@ -16,14 +16,14 @@
 std::tuple<float, float> getCoords(int width, int height, std::string input, std::string keys, int8_t length) {
     auto x = 0,
       y = 0;
-    qDebug() << "input " << QString::fromStdString(input) << " input.length " << input.length();
+//    qDebug() << "input " << QString::fromStdString(input) << " input.length " << input.length();
     if (input.length() == 0) return std::make_tuple(x, y);
     std::size_t cidx = keys.find(input[0]);
     auto cols = cidx % length;
     auto rows = std::floor(cidx / length);
     x = cols * (width / length);
     y = rows * (height / length);
-    qDebug() << "rows " << rows << " cols " << cols;
+//    qDebug() << "rows " << rows << " cols " << cols;
     auto subx = 0,
       suby = 0;
     std::tie(subx, suby) = getCoords(
@@ -32,10 +32,10 @@ std::tuple<float, float> getCoords(int width, int height, std::string input, std
       input.length() > 0 ? input.substr(1) : "",
       keys,
       length);
-    qDebug() << "subx " << subx << " suby " << suby;
+//    qDebug() << "subx " << subx << " suby " << suby;
     x += subx;
     y += suby;
-    qDebug() << "x " << x << " y " << y;
+//    qDebug() << "x " << x << " y " << y;
     return std::make_tuple(x, y);
 }
 
@@ -51,12 +51,26 @@ int main(int argc, char *argv[])
     QQmlComponent component(&qengine, "qrc:/main.qml");
     QObject *object = component.create(objectContext);
 
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+
     const auto data      = toml::parse("options.toml");
     const auto permutation = toml::find(data, "permutation");
     const auto keys    = toml::find<std::string>(permutation, "keys");
     const auto length    = toml::find<std::int8_t>(permutation, "length");
-    const auto key_bindings = toml::find(data, "key_bindings");
-    const auto hotkey_setting    = toml::find<std::string>(key_bindings, "hotkey");
+    const auto shortcuts_data = toml::find(data, "shortcuts");
+    const auto cancel_shortcut = toml::find<std::string>(shortcuts_data, "cancel");
+    std::vector<std::string> operations = {
+        "move", "left_click", "middle_click", "right_click", "drag_left", "drag_right"
+    };
+    std::map<std::string, std::string> shortcuts;
+    for(auto& operation : operations)
+    {
+        shortcuts[operation] = toml::find<std::string>(shortcuts_data, operation);
+    }
+
 
     std::vector<QHotkey *> hotkeys;
 
@@ -65,58 +79,86 @@ int main(int argc, char *argv[])
     canvas->setProperty("keys", QString::fromStdString(keys));
     canvas->setProperty("length", length);
 
-    QScreen *screen = QGuiApplication::primaryScreen();
-    QRect  screenGeometry = screen->geometry();
-    int height = screenGeometry.height();
-    int width = screenGeometry.width();
+    auto draw = [length, canvas](int x, int y, int width, int height, int n)
+    {
+        canvas->setProperty("xOffset", x);
+        canvas->setProperty("yOffset", y);
+        auto newWidth = width / pow(length, n);
+        auto newHeight = height / pow(length, n);
+        qDebug() << "length " <<  length << " n " << n;
+        qDebug() << "width " <<  width << " height " << newHeight;
+        qDebug() << "newWidth " <<  newWidth << " newHeight " << newHeight;
+        canvas->setProperty("_width", QVariant::fromValue(newWidth));
+        canvas->setProperty("_height", QVariant::fromValue(newHeight));
+        QMetaObject::invokeMethod(canvas, "requestDraw");
+    };
+
+    std::string operation = "";
     std::string input = "";
-    std::vector<std::string> prefixes = {
-// :/      "",
-        "shift+",
-        "ctrl+",
-        "ctrl+shift+",
-        "alt+"};
-    for (std::size_t p = 0; p < prefixes.size(); ++p) {
-        auto prefix = prefixes[p];
-        for (ulong i = 0; i < keys.length(); ++i) {
-            std::string keyChar = keys.substr(i, 1);
-            QHotkey *ahotkey = new QHotkey(QKeySequence(QString::fromStdString(prefix + keyChar)), false, &app);
-            qDebug() << "ahotkey: " << QString::fromStdString(prefix + keyChar) << " " << ahotkey << (ahotkey == NULL);
-            hotkeys.push_back(ahotkey);
-            qDebug() << "hotkeys[i]: " <<  hotkeys[i];
-            QObject::connect(hotkeys[i], &QHotkey::activated, qApp, [&, keyChar, prefix](){
-                qDebug() << "input Hotkey Activated." <<  QString::fromStdString(prefix + keyChar) << " " << QString::fromStdString(keyChar);
-                input += keyChar;
-                auto x = 0, y = 0;
-                std::tie(x, y) =  getCoords(width , height, input, keys, length);
-                canvas->setProperty("xOffset", x);
-                canvas->setProperty("yOffset", y);
-                auto newWidth = width / pow(length, input.length());
-                auto newHeight = height / pow(length, input.length());
-                qDebug() << "newWidth " <<  newWidth << " newHeight " << newHeight;
-                canvas->setProperty("_width", QVariant::fromValue(newWidth));
-                canvas->setProperty("_height", QVariant::fromValue(newHeight));
-                QMetaObject::invokeMethod(canvas, "requestDraw");
+
+    qDebug() << "creating cancel QHotkey";
+    auto cancel_hotkey = new QHotkey(QKeySequence(QString::fromStdString(cancel_shortcut)), false, &app);
+    QObject::connect(cancel_hotkey, &QHotkey::activated, qApp, [&]() {
+        operation = "";
+        input = "";
+        draw(0, 0, width, height, length);
+    });
+
+    auto click = [](_MMMouseButton button)
+    {
+        toggleMouse(true, button);
+        toggleMouse(false, button);
+    };
+    for (ulong i = 0; i < keys.length(); ++i) {
+        std::string keyChar = keys.substr(i, 1);
+        QHotkey *ahotkey = new QHotkey(QKeySequence(QString::fromStdString(keyChar)), false, &app);
+        qDebug() << "ahotkey: " << QString::fromStdString(keyChar) << " " << ahotkey << (ahotkey == NULL);
+        hotkeys.push_back(ahotkey);
+        qDebug() << "hotkeys[i]: " <<  hotkeys.at(i);
+        QObject::connect(hotkeys.at(i), &QHotkey::activated, qApp, [&, keyChar](){
+            qDebug() << "input Hotkey Activated." <<  QString::fromStdString(keyChar) << " " << QString::fromStdString(keyChar);
+            input += keyChar;
+            auto x = 0, y = 0;
+            std::tie(x, y) =  getCoords(width , height, input, keys, length);
+            if (input.length() == length) {
+                input = "";
                 auto point = MMSignedPointMake(x, y);
                 moveMouse(point);
-//                toggleKey(0, false, MOD_SHIFT);
-//                toggleMouse(true, LEFT_BUTTON);
-//                toggleMouse(false, LEFT_BUTTON);
-            });
-        }
+                if (operation == "left_click")
+                    click(LEFT_BUTTON);
+                else if (operation == "right_click")
+                    click(RIGHT_BUTTON);
+                else if (operation == "middle_click")
+                    click(CENTER_BUTTON);
+                operation = "";
+                canvas->setProperty("visible", false);
+                for (ulong i = 0; i < hotkeys.size(); ++i) {
+                    hotkeys.at(i)->setRegistered(false);
+                }
+                cancel_hotkey->setRegistered(false);
+                x = 0, y = 0;
+            }
+            draw(x, y, width, height, input.length());
+        });
     }
 
-    auto hotkey = new QHotkey(QKeySequence(QString::fromStdString((hotkey_setting))), true, &app);
 
-    QObject::connect(hotkey, &QHotkey::activated, qApp, [&]() {
-        qDebug() << "trigger Hotkey Activated." << hotkey->keyCode();
-        canvas->setProperty("visible", true);
-        qDebug() << "keys.length() * prefixes.size()" << keys.length() * prefixes.size();
-        for (ulong i = 0; i < hotkeys.size(); ++i) {
-            qDebug() << "2 hotkeys[i]: " <<  hotkeys[i];
-            hotkeys[i]->setRegistered(true);
-        }
-    });
+    for(auto& shortcut : shortcuts)
+    {
+        qDebug() << "regestering trigger Hotkey" << QString::fromStdString(shortcut.second);
+        auto shortcut_hotkey = new QHotkey(QKeySequence(QString::fromStdString(shortcut.second)), true, &app);
+        QObject::connect(shortcut_hotkey, &QHotkey::activated, qApp, [&, shortcut]() {
+            operation = shortcut.first;
+            qDebug() << "trigger Hotkey Activated." << QString::fromStdString(shortcut.second);
+            canvas->setProperty("visible", true);
+            for (ulong i = 0; i < hotkeys.size(); ++i) {
+                qDebug() << "2 hotkeys.at(i): " <<  hotkeys.at(i);
+                hotkeys.at(i)->setRegistered(true);
+            }
+            qDebug() << "enabling canecl QHotkey";
+            cancel_hotkey->setRegistered(true);
+        });
+    }
 
     return app.exec();
 }
